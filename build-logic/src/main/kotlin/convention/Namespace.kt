7e84e959b6d346aac367b7612561630f26f10ee3
SeonJeongk <83932309+SeonJeongk@users.noreply.github.com>
2026-04-28 03:00:56 +0900

    [CLIPY-35] chore: data:storage 모듈 생성

--- build-logic/src/main/kotlin/convention/Namespace.kt
+++ build-logic/src/main/kotlin/convention/Namespace.kt
@@ -4,7 +4,7 @@ import org.gradle.api.Project
 
 private const val BASE_NAMESPACE = "com.laxotters.clipy"
 
-internal fun Project.setNamespace(name: String) {
+fun Project.setNamespace(name: String) {
     androidExtension.namespace = if (name.isBlank()) {
         BASE_NAMESPACE
     } else {
