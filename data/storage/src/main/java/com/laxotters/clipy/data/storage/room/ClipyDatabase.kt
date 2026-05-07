0e03558ea3eafb981ecd9aae2679386da28d3986
SeonJeongk <83932309+SeonJeongk@users.noreply.github.com>
2026-05-08 01:18:58 +0900

    [CLIPY-40] feat: SessionViewStateDao 구현

--- data/storage/src/main/java/com/laxotters/clipy/data/storage/room/ClipyDatabase.kt
+++ data/storage/src/main/java/com/laxotters/clipy/data/storage/room/ClipyDatabase.kt
@@ -3,10 +3,12 @@ package com.laxotters.clipy.data.storage.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.laxotters.clipy.data.storage.room.dao.SessionDao
+import com.laxotters.clipy.data.storage.room.dao.SessionViewStateDao
 import com.laxotters.clipy.data.storage.room.entity.CaptureEntity
 import com.laxotters.clipy.data.storage.room.entity.DecisionEntity
 import com.laxotters.clipy.data.storage.room.entity.ItemEntity
 import com.laxotters.clipy.data.storage.room.entity.SessionEntity
+import com.laxotters.clipy.data.storage.room.entity.SessionViewStateEntity
 
 @Database(
     entities = [
@@ -14,10 +16,12 @@ import com.laxotters.clipy.data.storage.room.entity.SessionEntity
         ItemEntity::class,
         CaptureEntity::class,
         DecisionEntity::class,
+        SessionViewStateEntity::class,
     ],
     version = 1,
     exportSchema = true,
 )
 abstract class ClipyDatabase : RoomDatabase() {
     abstract fun sessionDao(): SessionDao
+    abstract fun sessionViewStateDao(): SessionViewStateDao
 }
