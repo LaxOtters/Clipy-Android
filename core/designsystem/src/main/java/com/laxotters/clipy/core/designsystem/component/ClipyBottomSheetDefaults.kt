e95390dff79037241fa1c6ba2a020bb093dae9ae
SeonJeongk <83932309+SeonJeongk@users.noreply.github.com>
2026-07-17 13:59:12 +0900

    [CLIPY-65] refactor: Legacy Theme 정리

--- core/designsystem/src/main/java/com/laxotters/clipy/core/designsystem/component/ClipyBottomSheetDefaults.kt
+++ core/designsystem/src/main/java/com/laxotters/clipy/core/designsystem/component/ClipyBottomSheetDefaults.kt
@@ -1,9 +1,11 @@
 package com.laxotters.clipy.core.designsystem.component
 
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.laxotters.clipy.core.designsystem.theme.ClipyTheme
 
 object ClipyBottomSheetDefaults {
     val HandleHeight: Dp = 32.dp
@@ -24,7 +26,8 @@ object ClipyBottomSheetDefaults {
         topEnd = 28.dp,
     )
     val SheetElevation: Dp = 16.dp
-    val HandleColor = Color(0xFFE4E4E7)
+    val HandleColor: Color
+        @Composable get() = ClipyTheme.colors.neutral.gray200
     const val CONTENT_FADE_IN_MILLIS = 500
     const val CONTENT_FADE_OUT_MILLIS = 500
 }
